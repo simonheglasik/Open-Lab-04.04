4812ced78fc8cff31935bf09a10bfe00510d3f54
simonheglasik <simonheglasik@gmail.com>
2019-11-06 17:25:48 +0100

    04.04

--- Open-Lab-04.04/PhoneNumbers.cs
+++ Open-Lab-04.04/PhoneNumbers.cs
@@ -6,7 +6,22 @@ namespace Open_Lab_04._04
     {
         public string FormatPhoneNumber(int[] numbers)
         {
-            throw new NotImplementedException();
+            string a = "";
+            string b = "";
+            string c = "";
+            for (int i = 0; i < 3; i++)
+            {
+                a = a +(numbers[i]);
+            }
+            for (int o = 3; o < 6; o++)
+            {
+                b = b + (numbers[o]);
+            }
+            for (int p = 6; p < 10; p++)
+            {
+                c = c + (numbers[p]);
+            }
+            return $"({a}) {b}-{c}";
         }
     }
 }
